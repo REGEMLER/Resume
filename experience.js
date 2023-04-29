50fe4192250580169e8afc6eaa033d92cc16e7aa
YarNight <yaroslav239@gmail.com>
2023-04-29 19:14:23 +0300

    add slider and acordeon

---
+++ experience.js
@@ -0,0 +1,58 @@
+const experience = [
+    {
+        "id": "1",
+        "name": "Game Memo",
+        "img": "assets/IMG/Memo.png",
+        "link": "https://regemler.github.io/Cards/"
+    },
+    {
+        "id": "2",
+        "name": "Clothing Store",
+        "img": "assets/IMG/ClothingStore.png",
+        "link": "https://regemler.github.io/ClothingStore/"
+    },
+    {
+        "id": "3",
+        "name": "Momentum",
+        "img": "assets/IMG/Mom.png",
+        "link": "https://github.com/REGEMLER/Momentum"
+    },
+    {
+        "id": "4",
+        "name": "Plant Shop",
+        "img": "assets/IMG/Plant.png",
+        "link": "https://regemler.github.io/Plant/"
+    },
+    {
+        "id": "5",
+        "name": "Restaurant",
+        "img": "assets/IMG/Restaurant.png",
+        "link": "https://regemler.github.io/Restaurant/"
+    },
+    {
+        "id": "6",
+        "name": "Game SOS",
+        "img": "assets/IMG/SOS.png",
+        "link": "https://regemler.github.io/SOS/"
+    },
+    {
+        "id": "7",
+        "name": "Traveland",
+        "img": "assets/IMG/Gaze.png",
+        "link": "https://regemler.github.io/Traveland/"
+    },
+    {
+        "id": "8",
+        "name": "Travel",
+        "img": "assets/IMG/Travel.png",
+        "link": "https://regemler.github.io/Travel/"
+    },
+    {
+        "id": "9",
+        "name": "Duck Hunt",
+        "img": "assets/IMG/Duck.png",
+        "link": "https://regemler.github.io/Duck-Hunt/"
+    },
+]
+
+export default experience;
--- script.js
+++ script.js
@@ -1,14 +1,132 @@
+import cards from './experience.js';
 const burger = document.getElementById(`burger`);
 const menu = document.getElementById(`header-menu`);
-burger.addEventListener(`click`, () =>{
-    if( menu.style.opacity === `1` && menu.style.height === `300px`){
-        menu.style.height = `0`;
-        menu.style.visibility = `hidden`;
-        menu.style.opacity = `0`;
+let isMenuOpen = false;
 
-    } else{
-        menu.style.height = `300px`;
-        menu.style.visibility = `visible`;
-        menu.style.opacity = `1`;
-        }
-    })
+function showMenu(){
+    const nav = document.querySelector(".header-nav");
+    menu.classList.add("header__menu__active");
+    burger.classList.add("burger__active"); 
+    document.body.style.overflowY = "hidden";
+    nav.classList.remove("overflow");
+    isMenuOpen = true;
+ }
+ 
+ 
+function hideMenu(){
+    menu.classList.remove("header__menu__active");
+    burger.classList.remove("burger__active"); 
+    document.body.style.overflowY = "";
+    isMenuOpen = false;
+}
+
+function moveMenuWithBurger(event){
+    event.stopPropagation();
+     if(menu.classList.contains("header__menu__active")){
+        menu.classList.add("header__menu__transition-slow");
+        hideMenu(); 
+        setTimeout(overflowNav,1100);
+        setTimeout(() => {
+          menu.classList.remove("header__menu__transition-slow");
+        },1100)
+     } else {
+         showMenu();
+     }
+}
+ 
+function hideMenuWithBody(event){
+     if(!isMenuOpen || event.target.closest(".header__menu")){
+         return; 
+     }   
+     menu.classList.add("header__menu__transition-slow");
+     hideMenu(); 
+     setTimeout(overflowNav,1100);
+     setTimeout(() => {
+       menu.classList.remove("header__menu__transition-slow");
+     },1100)
+  }
+ 
+function hideMenuWithLink(event){
+     if(event.target.closest(".header__item") && isMenuOpen){
+         menu.classList.add("header__menu__transition-fast");
+         hideMenu(); 
+         setTimeout(() => {
+          menu.classList.remove("header-menu__transition-fast");
+        },500)
+     }   
+}
+ 
+burger.addEventListener("click", moveMenuWithBurger); 
+document.body.addEventListener("click", hideMenuWithBody);
+menu.addEventListener("click", hideMenuWithLink);
+ 
+
+const arrowLeft = document.querySelector(".arrow-left");
+const arrowRight = document.querySelector(".arrow-right"); 
+const carusel = document.querySelector(".experience__inner"); 
+const leftItems = document.querySelector(".experience__back");
+const centreItems = document.querySelector(".experience__center");
+const rightItems = document.querySelector(".experience__forward");
+
+function createNewSlides(direction){
+    let newCardsArr = [...cards].filter(item => {
+        const card1 = centreItems.querySelector(".experience__card1");
+        const card2 = centreItems.querySelector(".experience__card2");
+        const card3 = centreItems.querySelector(".experience__card3");
+        return item.id !== card1.id && item.id !== card2.id && item.id !== card3.id; 
+      }).sort(() => Math.random() - 0.5);
+   
+    for(let i = 1; i < 4; i++){
+        const card = direction.querySelector(`.experience__card${i}`);
+        card.id = newCardsArr[i].id;
+        const img = card.querySelector("img");
+        img.src = newCardsArr[i].img;
+        img.alt = newCardsArr[i].name;
+        const a = card.querySelector("a");
+        a.textContent = newCardsArr[i].name;
+        a.href = newCardsArr[i].link;
+    }
+}
+   
+function moveLeft() { 
+    carusel.classList.add("carusel-left");
+    leftItems.classList.add("slide__animated");
+    arrowLeft.removeEventListener("click", moveLeft);
+    arrowRight.removeEventListener("click", moveRight);
+}
+   
+function moveRight() {
+    carusel.classList.add("carusel-right");
+    rightItems.classList.add("slide__animated");
+    arrowRight.removeEventListener("click", moveRight);
+    arrowLeft.removeEventListener("click", moveLeft);
+}
+   
+function animed(event){
+    if(event.animationName === "slider-left" || event.animationName === "slider-left-small"){
+        carusel.classList.remove("carusel-left"); 
+        leftItems.classList.remove("slide__animated");
+        rightItems.innerHTML = centreItems.innerHTML; 
+        centreItems.innerHTML = leftItems.innerHTML; 
+        createNewSlides(leftItems);
+   
+      } else{
+        carusel.classList.remove("carusel-right"); 
+        rightItems.classList.remove("slide__animated");
+        leftItems.innerHTML = centreItems.innerHTML; 
+        centreItems.innerHTML = rightItems.innerHTML; 
+        createNewSlides(rightItems);
+      }
+   
+    arrowLeft.addEventListener("click", moveLeft);
+    arrowRight.addEventListener("click", moveRight);
+}
+   
+arrowLeft.addEventListener("click", moveLeft);
+arrowRight.addEventListener("click", moveRight);
+carusel.addEventListener("animationend", animed); 
+
+createNewSlides(rightItems);
+createNewSlides(centreItems);
+createNewSlides(leftItems);
+
